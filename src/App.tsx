cf692ffd036947d393e9e45d805dfa7cdf9d3555
Atty Cronin <antoniajcronin@gmail.com>
2023-07-26 11:45:26 +0100

    Remove nav from useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { useEffect, useState } from "react";
-import { useFetch } from "./hooks/useFetch";
-import { useHero } from "./hooks/useHero";
-import { v4 as uuid } from "uuid";
-import "./App.css";
+
+import { useFetch } from './hooks/useFetch';
+import { useHero } from './hooks/useHero';
+import { v4 as uuid } from 'uuid';
+import './App.css';
 
 function App() {
   const [trips, setTrips] = useState(0);
-  const [nav, setNav] = useState<string[]>([]);
+  // const [nav, setNav] = useState<string[]>([]);
   const [boughtIt, setBoughtIt] = useState(false);
   const { data, isLoading, error } = useFetch();
   const { heroics, waitAMo, fail } = useHero();
 
+  const nav: Array<string> = ['trending', 'your orders', 'community'];
+
   useEffect(() => {
-    setNav(() => {
-      return ["trending", "your orders", "community"];
-    });
     if (heroics) if (data) setTrips(data.length);
   }, [data, heroics]);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setBoughtIt(true);
     if (e.currentTarget.value) {
-      const stuff = e.currentTarget.value.split("/");
+      const stuff = e.currentTarget.value.split('/');
       const experience = stuff[0];
       const cost = parseInt(stuff[1]);
       alert(
         `Okie dokie, so you've bought the '${experience}' excursion, at a cost of ${
           cost &&
-          cost.toLocaleString("en-GB", { style: "currency", currency: "GBP" })
+          cost.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' })
         }\rNice one 🚀`
       );
     }
@@ -45,22 +45,23 @@ function App() {
 
   return (
     <>
-      <header className="site-header">
-        <div className="site-identity">
+      <header className='site-header'>
+        <div className='site-identity'>
           <a>
             <img
-              className="eefe-logo"
-              src="/icons8-space-64.png"
-              alt="eefe spaceship logo"
+              className='eefe-logo'
+              src='/icons8-space-64.png'
+              alt='eefe spaceship logo'
             />
           </a>
           <h1>Extraterrestrial Excursions for Earthlings</h1>
         </div>
-        <ul className="go-somewhere">
-          {nav.map((place) => {
+
+        <ul className='main-menu'>
+          {nav.map((navLink) => {
             return (
-              <li key={place}>
-                <a>{place}</a>
+              <li key={navLink}>
+                <a href={navLink}> {navLink}</a>
               </li>
             );
           })}
@@ -70,22 +71,23 @@ function App() {
       {waitAMo && <p>...getting heroics...</p>}
       {heroics && (
         <div
-          className="hero-section"
+          className='hero-section'
           style={{
             background: `linear-gradient(0deg, rgba(10 10 10 / 90%), rgba(25 119 25 / 60%)), url(${heroics.image})`,
-            backgroundSize: "cover",
-            backgroundPosition: "50% 20%",
+            backgroundSize: 'cover',
+            backgroundPosition: '50% 20%',
           }}
         >
           <h3>We've been helping humanity traverse the universe for aeons.</h3>
           <p>
-            Now it's your turn to marvel at the wonders of the universe! But don't just take our word for it; check out
-            these smashing reviews from customers just like you!
+            Now it's your turn to marvel at the wonders of the universe! But
+            don't just take our word for it; check out these smashing reviews
+            from customers just like you!
           </p>
-          <ul className="testimonials">
+          <ul className='testimonials'>
             {heroics.testimonials.map(({ name, rating, spiel, social }) => {
               return (
-                <li key={name} className="test-card">
+                <li key={name} className='test-card'>
                   <h4>{name}</h4>
                   <a>{social}</a>
                   <p>{spiel}</p>
@@ -99,9 +101,9 @@ function App() {
 
       {isLoading && <h2>Loading...</h2>}
       {data && (
-        <div className="trips">
-          <h3>We found {trips} trips for you!</h3>{" "}
-          <ul className="trip-list">
+        <div className='trips'>
+          <h3>We found {trips} trips for you!</h3>{' '}
+          <ul className='trip-list'>
             {data.map(
               ({
                 id,
@@ -113,34 +115,34 @@ function App() {
                 isBookable,
               }) => {
                 return (
-                  <li key={id} className="trip-card">
-                    <h3 className="trip-name grid-col-span-2">{tripName}</h3>
-                    <p className="trip-description grid-col-span-2">
+                  <li key={id} className='trip-card'>
+                    <h3 className='trip-name grid-col-span-2'>{tripName}</h3>
+                    <p className='trip-description grid-col-span-2'>
                       {description}
                     </p>
                     <img
-                      className="trip-thumbnail"
+                      className='trip-thumbnail'
                       src={imageUrl}
                       alt={`representation of ${tripName}`}
                     />
-                    <p className="trip-duration">
+                    <p className='trip-duration'>
                       Duration: {lengthInDays} day
-                      {lengthInDays > 1 && "s"}
+                      {lengthInDays > 1 && 's'}
                     </p>
-                    <p className="trip-price">
-                      Price:{" "}
-                      {cost.toLocaleString("en-GB", {
-                        style: "currency",
-                        currency: "GBP",
+                    <p className='trip-price'>
+                      Price:{' '}
+                      {cost.toLocaleString('en-GB', {
+                        style: 'currency',
+                        currency: 'GBP',
                       })}
                     </p>
                     <button
-                      className="grid-col-span-2 trip-book-button"
+                      className='grid-col-span-2 trip-book-button'
                       disabled={!isBookable}
                       onClick={handleClick}
                       value={`${tripName}/${cost}`}
                     >
-                      {isBookable ? "book now" : "fully booked"}
+                      {isBookable ? 'book now' : 'fully booked'}
                     </button>
                   </li>
                 );
